1ef8f0dba44b077edc62f854a7d5b54f972dad1a
Amrender kuamr <amrenderk66@gmail.com>
2026-07-28 22:32:13 +0530

    update todo

--- Project4/src/components/Todo.jsx
+++ Project4/src/components/Todo.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Todo = () => {
   return (
-    <div>
-      
+    <div className='h-screen'>
+      <div className='bg-red-100'>
+        <h1>This is a Todo</h1>
+      </div>
     </div>
   )
 }
