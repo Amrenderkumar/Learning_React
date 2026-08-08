b11d63cc1c87b9fb113d10facf24a4de13ae14b6
Amrender kuamr <amrenderk66@gmail.com>
2026-08-08 23:23:53 +0530

    change commoit

--- Project4/src/components/Todo.jsx
+++ Project4/src/components/Todo.jsx
@@ -7,7 +7,7 @@ const Todo = () => {
       <div className='bg-red-100'>
         <h1>This is a Todo</h1>
       </div>
-      <div>
+      <div className='bg-red-200'>
         <h1>hello</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, unde ab hic possimus libero pariatur voluptatibus ipsum corrupti molestias! Exercitationem libero pariatur quasi, necessitatibus tenetur ipsam debitis totam quam magnam?</p>
       </div>
