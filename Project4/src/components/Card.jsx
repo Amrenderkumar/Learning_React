52f02adb2d1246bdcce190aced367a27358f39ed
Amrender kuamr <amrenderk66@gmail.com>
2026-08-11 22:21:50 +0530

    new commit card

--- Project4/src/components/Card.jsx
+++ Project4/src/components/Card.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Card = () => {
   return (
     <div>
-      
+      <div className='bg-red-400 text-black p-4'>
+        <h1>card title</h1>
+      <div>
+        <div>card content</div>
+      </div>
+      </div>
     </div>
   )
 }
