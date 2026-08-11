514c51b9cc86055e346c45106aa425f7e3492fe4
Amrender kuamr <amrenderk66@gmail.com>
2026-08-11 22:23:36 +0530

    change class name and its properties

--- Project4/src/components/Card.jsx
+++ Project4/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = () => {
     <div>
       <div className='bg-red-400 text-black p-4'>
         <h1>card title</h1>
-      <div>
+      <div className='p-1 size-2 bg-amber-200 absolute'>
         <div>card content</div>
       </div>
       </div>
