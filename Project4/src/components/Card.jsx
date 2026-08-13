ac8f8c0f3835927e4c1aefc37f804ec6ae4b0736
Amrender kuamr <amrenderk66@gmail.com>
2026-08-13 23:25:53 +0530

    ..

--- Project4/src/components/Card.jsx
+++ Project4/src/components/Card.jsx
@@ -5,9 +5,9 @@ const Card = () => {
     <div>
       <div className='bg-red-400 text-black p-4'>
         <h1>card title</h1>
-      <div className='p-1 size-2 bg-amber-200 absolute'>
-        <div className='text-2xl font-black'>card content</div>
-      </div>
+        <div className='p-1 size-2 bg-amber-200 absolute'>
+          <div className='text-2xl font-black'>card content</div>
+        </div>
       </div>
     </div>
   )
