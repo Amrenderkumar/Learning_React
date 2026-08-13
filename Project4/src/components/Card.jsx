4f91fd71d3bcfafb19d08aaa31e50858e2236857
Amrender kuamr <amrenderk66@gmail.com>
2026-08-13 23:27:05 +0530

    change the card

--- Project4/src/components/Card.jsx
+++ Project4/src/components/Card.jsx
@@ -8,6 +8,9 @@ const Card = () => {
         <div className='p-1 size-2 bg-amber-200 absolute'>
           <div className='text-2xl font-black'>card content</div>
         </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis eum possimus laboriosam ratione eos quidem, repellat unde atque. Laboriosam excepturi debitis odio enim quaerat voluptates deleniti, eaque nemo sunt voluptatibus!</p>
+        </div>
       </div>
     </div>
   )
