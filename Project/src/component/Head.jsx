0da61c96e9d0316e66d8eb607fcd4f22f2ac0cb4
Amrender kuamr <amrenderk66@gmail.com>
2026-08-16 23:07:48 +0530

    we have to use the multiple time tailwind css propery

--- Project/src/component/Head.jsx
+++ Project/src/component/Head.jsx
@@ -17,6 +17,9 @@ const Head = () => {
             <div>
                 <img src="https://wallpapers.com/images/hd/ikea-in-shopping-mall-6kggi26himgb9g07.jpg" alt="Placeholder" className=' rounded-lg shadow-lg border-4 border-white ' />
             </div>
+             <div>
+                <img src="https://wallpapers.com/images/hd/ikea-in-shopping-mall-6kggi26himgb9g07.jpg" alt="Placeholder" className='blur-3xl rounded-lg shadow-lg border-4 border-white ' />
+            </div>
         </div>
     )
 }
