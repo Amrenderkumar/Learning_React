cba33f109463436700ed1a579e5064166b90a272
Amrender kuamr <amrenderk66@gmail.com>
2026-08-27 23:04:33 +0530

    change it

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <div>
       <Head />
+      <main>
+        <h1>Welcome to My App</h1>
+        <p>This is the main content area.</p>
+      </main>
     </div>
   )
 }
