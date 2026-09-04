91968001d5a6d6ab88905e8d4813b06c80b3abb4
Amrender kuamr <amrenderk66@gmail.com>
2026-09-04 22:57:54 +0530

    add on the paragraph

--- Project2/src/components/Home.jsx
+++ Project2/src/components/Home.jsx
@@ -40,7 +40,7 @@ export default function Home() {
                 <img className="sm:w-96 w-48" src="https://static.vecteezy.com/system/resources/previews/021/923/191/original/programmer-coding-concept-man-writing-code-at-laptop-develops-program-website-or-application-programmers-and-it-specialists-in-workplace-programming-code-with-popular-programming-languages-vector.jpg" alt="image2" />
             </div>
             <div>
-                <p>heloo</p>
+                <p>This is the page of Content</p>
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
