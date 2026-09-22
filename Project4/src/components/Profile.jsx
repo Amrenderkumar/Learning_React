5813ce3ea88e9e9539dd2043f7c56f1d73cf01db
Amrender kuamr <amrenderk66@gmail.com>
2026-09-22 22:31:24 +0530

    create a new file

---
+++ Project4/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='bg-gray-400 text-black'>
+        <div className='bg-red-400 text-black p-4'>
+            <h1>Profile</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
